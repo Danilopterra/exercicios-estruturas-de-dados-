a0aa84c8568a3a9169ecd2d3665dbf178f65c885
Danilopterra <36747469+Danilopterra@users.noreply.github.com>
2018-02-22 20:09:24 -0300

    Add files via upload

---
+++ lista dia 22.02.2018/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+//Complemente o programa abaixo para que 
+//escreva o número 10 na tela, utilizando x, p.
+// #include <stdio.h> void main() { int x, *p; p = &x; x = 10; } 
+
+void main() { 
+	int x, *p;
+	p = &x; 
+	x = 10;
+	 
+}
+
